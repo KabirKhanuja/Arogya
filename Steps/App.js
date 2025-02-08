ef1d21b2a2c74c61251d777ff2b88680459b0a6b
KabirKhanuja <kabirkhanuja@gmail.com>
2025-02-08 18:36:34 +0530

    updated bro

---
+++ Steps/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import StepTracker from './StepTracker';  // Import the StepTracker component
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StepTracker />  {/* Render the StepTracker component */}
+    </SafeAreaView>
+  );
+};
+
+export default App;
